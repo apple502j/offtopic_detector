f84e0b745357e2cf3705d2e74476665a4a531bfa
Shun Sugiyama <mathgeekjp@gmail.com>
2018-11-16 22:00:36 +0900

    update some files

--- eval.py
+++ eval.py
@@ -3,11 +3,13 @@ from sklearn.model_selection import cross_val_score
 from sklearn.utils import shuffle
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import CountVectorizer
+
 
 def cross_val(clf, datafile="content.csv", cv=5):
     df = shuffle(pd.read_csv(datafile))
     data = contents2characters(df['content'])
-    vec = create_vectorizer(data)
+    vec = CountVectorizer(strip_accents=None, stop_words=None, analyzer="char").fit(data)
     vec_transformed = vec.transform(data)
     return cross_val_score(clf, vec_transformed, df['label'], scoring="roc_auc", cv=cv)
 
--- predictor.py
+++ predictor.py
@@ -25,16 +25,6 @@ class Predictor:
 
 
 if __name__ == "__main__":
+    import sys
     predictor = Predictor()
-    print(predictor.predict("https://qiita.com/taku910/items/7e52f1e58d0ea6e7859c"))
-    print(predictor.predict("https://qiita.com/Yuta_Yamamoto/items/fbeb7b31173b3e787fc2"))
-    print(predictor.predict("https://qiita.com/riversun/items/29d5264480dd06c7b9fb"))
-    print(predictor.predict("https://qiita.com/_EnumHack/items/3d7d50c43523c71ab307"))
-    print(predictor.predict("https://qiita.com/tani_AI_Academy/items/3edc5effeb386ae3caa9"))
-    print(predictor.predict("https://qiita.com/charmston/items/df31a419a4e57ebe86ba"))
-    print(predictor.predict("https://qiita.com/rana_kualu/items/afe544b0f5680e81fabc"))
-    print(predictor.predict("https://qiita.com/qiitadaisuki/items/2160a390ce91283707a1"))
-    print(predictor.predict("https://qiita.com/Kosuke-Szk/items/4b74b5cce84f423b7125"))
-    print(predictor.predict("https://qiita.com/Y_F_Acoustics/items/6742a0a8ad6b37a0f4f5"))
-    print(predictor.predict("https://qiita.com/ozikot/items/f7e5c346e631de067efb"))
-    print(predictor.predict("https://qiita.com/kurogelee/items/1c081a5a7e209e81921c"))
+    print(predictor.predict(sys.argv[1]))
